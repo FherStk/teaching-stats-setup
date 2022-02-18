65d53f6e80291786beba0b4dac4bba13efa90fa8
FherStk <porrino.fernando@gmail.com>
2022-02-18 13:29:06 +0100

    test

--- install-localhost.sh
+++ install-localhost.sh
@@ -88,7 +88,7 @@ if [ $(runuser -l postgres -c 'psql -c "\du teaching-stats" | cut -d \| -f 1 | g
     echo "${LCYAN}Creating the 'teaching-stats' database user:${NC}"
     pwd_req() #stores in pwd var
 
-    runuser -l postgres -c 'psql -e -c "CREATE USER \"teaching-stats\" WITH PASSWORD '${pwd}'"'
+    runuser -l postgres -c 'psql -e -c "CREATE USER \"teaching-stats\" WITH PASSWORD \'${pwd}\'"'
     runuser -l postgres -c 'psql -e -c "ALTER DATABASE \"teaching-stats\" OWNER TO \"teaching-stats\""'
 fi
 
