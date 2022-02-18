114f25345de4c9a9164d85ff974061befcadf9dd
FherStk <porrino.fernando@gmail.com>
2022-02-18 11:16:33 +0100

    Building

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
+# Terminal colors:
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 LCYAN='\033[1;36m'
@@ -45,3 +57,6 @@ requirement python3
 
 #echo ""
 #requirement psycopg2-binary==2.9.3
+
+echo ""
+echo "${GREEN}Finish!${NC}"
