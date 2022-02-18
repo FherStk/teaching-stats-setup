ea9a1319a2c3b987e22a273437d8be185d734c61
FherStk <porrino.fernando@gmail.com>
2022-02-18 11:06:44 +0100

    Building

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
 requirement()
 {
   if [ $(dpkg-query -W -f='${Status}' $0 2>/dev/null | grep -c "ok installed") -eq 0 ];
@@ -9,11 +14,11 @@ requirement()
 }
 
 echo ""
-echo "Setup for Teaching Stats: Install (v1.0.0)"
-echo "Copyright © 2022: Marcos Alcocer Gil"
-echo "Copyright © 2022: Fernando Porrino Serrano"
-echo "Under the AGPL license: https://github.com/FherStk/teaching-stats-setup/blob/main/LICENSE"
-echo ""
+echo "${YELLOW}Setup for Teaching Stats:${NC} Install (v1.0.0)"
+echo "${YELLOW}Copyright © 2022:${NC} Marcos Alcocer Gil"
+echo "${YELLOW}Copyright © 2022:${NC} Fernando Porrino Serrano"
+echo "${YELLOW}Under the AGPL license:${NC} https://github.com/FherStk/teaching-stats-setup/blob/main/LICENSE"
+
 echo ""
 echo "Updating repo list"
 apt update
@@ -23,3 +28,18 @@ requirement apache2
 
 echo ""
 requirement python3
+
+# echo ""
+# requirement libpq-dev python-dev
+
+# echo ""
+# requirement python3-pip
+
+# echo ""
+# requirement django==4.0.1
+
+# echo ""
+# requirement django-allauth==0.47.0
+
+#echo ""
+#requirement psycopg2-binary==2.9.3
