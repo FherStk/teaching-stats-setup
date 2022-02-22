fffcbfa1c033f106e70779411e81255ad35603bd
FherStk <porrino.fernando@gmail.com>
2022-02-22 09:25:05 +0100

    test

--- install-localhost.sh
+++ install-localhost.sh
@@ -77,7 +77,7 @@ BBDD_user(){
     echo "${LCYAN}Creating the '${BBDD}' database user:${NC}"
     PWD_req() #stores in PWD var
 
-    runuser -l postgres -c "psql -e -c \"CREATE USER \"${BBDD}\" WITH PASSWORD '"'"'${PWD}'"'"';\""  #'"'"' means ' -> https://stackoverflow.com/a/1250279
+    runuser -l postgres -c "psql -e -c 'CREATE USER \"${BBDD}\" WITH PASSWORD '"'"'${PWD}'"'"';'"  #'"'"' means ' -> https://stackoverflow.com/a/1250279
     runuser -l postgres -c "psql -e -c 'ALTER DATABASE \"${BBDD}\" OWNER TO \"${BBDD}\";'"
   fi
 }
