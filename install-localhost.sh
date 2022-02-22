2b5315bf6d7f0e627f7a21bef8e6d16be8c48001
FherStk <porrino.fernando@gmail.com>
2022-02-22 12:16:02 +0100

    test

--- install-localhost.sh
+++ install-localhost.sh
@@ -179,11 +179,9 @@ setup_django()
     echo -e "Please, provide the password for the ${CYAN}${BBDD}${NC} django superuser:"
     read -s PASS          
   fi
-
-  DJANGO_SUPERUSER_USERNAME=${BBDD} \
+  
   DJANGO_SUPERUSER_PASSWORD=${PASS} \
-  DJANGO_SUPERUSER_EMAIL="" \
-  python3 manage.py createsuperuser --noinput
+  python3 manage.py createsuperuser --noinput --username ${BBDD} -- email somemail@somedomain.com
 
   cd $HOME/${CURRENT}
 }
