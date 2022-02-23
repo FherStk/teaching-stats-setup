a006d566d69049035d9fcf8e785fb9ee92d6b055
FherStk <porrino.fernando@gmail.com>
2022-02-23 13:32:43 +0100

    fix

--- install-localhost.sh
+++ install-localhost.sh
@@ -206,7 +206,7 @@ setup_gauth(){
     
     echo -e "${ORANGE}Please, provide an ${CYAN}IP address${ORANGE} if you want to override the default localhost (127.0.0.1), otherwise leave it in blank${ORANGE}:${NC}"
     read HOST
-    if [ -z "$IP" ]; then    
+    if [ -z "$HOST" ]; then    
       HOST="127.0.0.1"
     fi
 
