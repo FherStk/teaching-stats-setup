dfa23e700da429b36ec94bb7b9c9eee9b222538e
FherStk <porrino.fernando@gmail.com>
2022-03-16 11:40:56 +0100

    fix

--- start.sh
+++ start.sh
@@ -26,29 +26,10 @@ echo
 echo "You can access to the survey stats through:"
 echo "    http://${IPv4}:8000/resultats"
 echo "    http://127.0.0.1:8000/resultats"
+echo 
 
 cd /var/www/teaching-stats
 python3 manage.py runserver 0.0.0.0:8000
-# CURRENT=${PWD##*/}
-# cd /var/www/teaching-stats
-# python3 manage.py runserver 0.0.0.0:8000  > /dev/null 2>&1 &  #use '0.0.0.0:8000' when running within a container, in order to allow remote connections
-# PID=$!  
-
-# cd $HOME/${CURRENT}
-# rm -f teaching-stats.pid
-# touch teaching-stats.pid
-# echo ${PID} > teaching-stats.pid
-
-# IPv4=$(hostname -I | cut -d' ' -f1)
-
-# echo 
-# echo "You can access to the survey system through:"
-# echo "    http://${IPv4}:8000/"
-# echo "    http://127.0.0.1:8000/"
-# echo 
-# echo "You can access to the survey stats through:"
-# echo "    http://${IPv4}:8000/resultats"
-# echo "    http://127.0.0.1:8000/resultats"
 
 trap : 0
 echo ""
