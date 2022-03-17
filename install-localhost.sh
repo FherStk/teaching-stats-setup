0aed2253b5eda88f6f5fb088639989b393966726
FherStk <porrino.fernando@gmail.com>
2022-03-17 12:53:13 +0100

    fix

--- install-localhost.sh
+++ install-localhost.sh
@@ -499,7 +499,7 @@ metabase_bbdd()
     
     runuser -l postgres -c "psql -e -c 'ALTER DATABASE \"${BBDD}-metabase\" OWNER TO \"${BBDD}\";'"
     runuser -l postgres -c "psql -d \"${BBDD}\" -e -c 'ALTER SCHEMA \"public\" OWNER TO \"${BBDD}\";'"
-    runuser -l postgres -c "psql -d \"${BBDD}\" -e -c 'CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public; GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO \"${BBDD}\";ALTER USER \"${BBDD}\" SET current_schema=public'"    
+    runuser -l postgres -c "psql -d \"${BBDD}\" -e -c 'CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public; GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO \"${BBDD}\";'"    
 
     echo 
     echo -e "${CYAN}Importing the SQL Dump into the ${LCYAN}metabase${CYAN} database:${NC}" 
